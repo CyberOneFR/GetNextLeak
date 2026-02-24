1edbbb46f47f17cb67e2f36b05d378bb0d5d702d
CyberOneFR <erwoan.thebault@gmail.com>
2026-02-24 17:57:44 +0000

    cleaned and patched

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ethebaul <ethebaul@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 17:31:36 by ethebaul          #+#    #+#             */
-/*   Updated: 2026/02/24 17:53:00 by ethebaul         ###   ########.fr       */
+/*   Updated: 2026/02/24 17:56:37 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 # include <fcntl.h>
 # include <unistd.h>
 
-typedef size_t			t_sz;
-typedef volatile t_sz	(*t_read_file)(t_sz, t_sz, t_sz, t_sz, t_sz, t_sz);
-typedef volatile int	(*t_get_size)(int, struct stat *);
+typedef size_t	t_sz;
+typedef t_sz	(*t_read_file)(t_sz, t_sz, t_sz, t_sz, t_sz, t_sz);
+typedef int		(*t_get_size)(int, struct stat *);
 
 int		get_size(int fd, struct stat *stat);
 void	*read_file(size_t size, int fd);
